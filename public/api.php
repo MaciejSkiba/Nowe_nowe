b2c96b0026488b304ff3b57340ef6b42169d0dd5
Student <student@d17-l427-pc-19.ipa.ki.agh.edu.pl>
2020-01-27 09:28:46 +0100

    prawie dodaje do bazy

--- public/api.php
+++ public/api.php
@@ -4,6 +4,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+class MyDB extends SQLite3 {
+function __construct() {
+ $this->open('../participants.db');
+}
+}
+$db = new MyDB();
+if(!$db) {
+echo $db->lastErrorMsg();
+exit();
+}
+
 $app = new \Slim\App;
 $app->get(
     '/hello/{name}',
@@ -16,12 +27,17 @@ $app->get(
 
 $app->get(
     '/api/participants',
-    function (Request $request, Response $response, array $args) {
-        $participants = [
-            ['id' => 1, 'firstname' => 'John', 'lastname' => 'Doe'],
-            ['id' => 2, 'firstname' => 'Kate', 'lastname' => 'Pig'],
-            ['id' => 3, 'firstname' => 'Chris', 'lastname' => 'Lua'],
-        ];
+    function (Request $request, Response $response, array $args) use ($db){
+
+ $participants = [];
+
+ $sql = "SELECT id, firstname, lastname FROM participant";
+$ret = $db->query($sql);
+while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+    $participants[]= $row;
+}
+$db->close();
+
         return $response->withJson($participants);
     }
 );
